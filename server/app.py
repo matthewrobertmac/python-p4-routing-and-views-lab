35b4383a0ee65c9cc74c68137e3b0de5099bbb2a
Matt MacFarlane <tidydata@outlook.com>
2023-05-21 02:18:55 -0400

    think im done

--- server/app.py
+++ server/app.py
@@ -6,3 +6,22 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+  return <h1>Python Operations with Flask Routing and Views</h1>
+
+@app.route('/print/parameter')
+def print_string(str):
+  print(str)
+  return <h1>{len(str)}</h1>
+
+@app.route('/count/parameter')
+def count(int):
+  for i in range(int):
+    return \ni
+
+@app.route('/math/<num1>/<operation>/<num2>')
+
+
+
